7ffa56b3b2b380dabbc4b04fea0e5b367a135ef6
frankwoods <liamtbo@gmail.com>
2024-09-04 13:40:35 -0700

    update

--- test1.py
+++ test1.py
@@ -1,10 +1,1 @@
-import torch
-
-a = torch.tensor([1,2,3,4], dtype=torch.float, requires_grad=True)
-b = torch.tensor([5,6,7,8], dtype=torch.float, requires_grad=True)
-c = a * 5
-e = b * 5
-d = (c+e).sum()
-
-d.backward()
-print(a.grad)
+print("buttcheak")

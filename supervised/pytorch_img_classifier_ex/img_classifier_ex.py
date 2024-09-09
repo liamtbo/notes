6978da16c783d08048c3c69a2ed9489716c00f82
frankwoods <liamtbo@gmail.com>
2024-09-09 12:09:17 -0700

    squashed commit

---
+++ supervised/pytorch_img_classifier_ex/img_classifier_ex.py
@@ -0,0 +1,172 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+transform = transforms.Compose( # applies transofrmations in sequence
+    # converts PIL image or Numpy of [0,255] -> tensor of [0,1]
+    [transforms.ToTensor(),
+     # 1st arg is mean, 2nd arg is std wrt the channels (rgb)
+     # mapping from [0,1] -> [-1,1] range
+     # normalized_val = (orig - mean) / std
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) 
+
+batch_size = 4
+# loads 50k images
+trainset = torchvision.datasets.CIFAR10(root='./notes/supervised/pytorch_img_classifier_ex/data', train=True,
+                                        download=True, transform=transform)
+# creates iterator over the data set
+## num_workers is num of subprocesses used to load data - parallelism
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+# loads 10k images
+testset = torchvision.datasets.CIFAR10(root='./notes/supervised/pytorch_img_classifier_ex/data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# get some random training images
+dataiter = iter(trainloader) # setup data to be iterated over
+images, labels = next(dataiter) # grabs first iteration of data
+
+# show images
+imshow(torchvision.utils.make_grid(images)) # display images
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+# conv2d(in_channels, out_channels, kernal_size)
+    # in_channels - 3 for rgb
+    # out_channels - number of filter (kernals) applied. each filter creates one feature map
+    # kernal_size - int for an int x int filter
+# TODO reveiw conv layers, figure out 16 * 5 * 5
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 12, 5)
+        self.pool = nn.MaxPool2d(2, 2) # pool the conv layers to reduce dim
+        self.conv2 = nn.Conv2d(12, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net().to(device)
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss() # use cross entropy for classification problems
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) # for grad descent
+
+
+for epoch in range(2):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        # inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+# save model
+PATH = "./notes/supervised/pytorch_img_classifier_ex/cifar_net.pth"
+torch.save(net.state_dict(), PATH)
+
+# test data
+dataiter = iter(testloader)
+images, labels = next(dataiter)
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+net = Net()
+net.load_state_dict(torch.load(PATH, weights_only=True))
+outputs = net(images)
+
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
+                              for j in range(4)))
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
+# classes that performed well and those that didnt
+# prepare to count predictions for each class
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+# again no gradients needed
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        # collect the correct predictions for each class
+        for label, prediction in zip(labels, predictions):
+            if label == prediction:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+
+
+# print accuracy for each class
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
